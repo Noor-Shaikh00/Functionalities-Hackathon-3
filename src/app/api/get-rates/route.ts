9ffdeb639d874138c8f59877ef5a55691832e6c8
Noor-Shaikh00 <developernoorshaikh00@gmail.com>
2025-02-02 12:38:51 +0500

    push

--- src/app/api/get-rates/route.ts
+++ src/app/api/get-rates/route.ts
@@ -19,8 +19,8 @@ export async function POST(req: NextRequest) {
     }
 
     const shipFromAddress: Address = {
-      name: "Muhammad Ubaid Hussain",
-      phone: "+92 3172077696",
+      name: "Noor Shaikh",
+      phone: "+92 313 2272 190",
       addressLine1: "456 Oak Avenue",
       addressLine2: "Suite 200",
       cityLocality: "Los Angeles",
